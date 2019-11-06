1aeeb44b9e90edc9c664f9da16fc8828f136b94e
Hizenhawer <tomek.herzyk@gmail.com>
2019-11-06 15:36:37 +0100

    Baby steps

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+image_index = 7777 # You may select anything up to 60,000
+print(y_train[image_index]) # The label is 8
+plt.imshow(x_train[image_index], cmap='Greys')
+plt.show()
